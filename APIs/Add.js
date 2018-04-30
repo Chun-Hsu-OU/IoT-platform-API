2c07fdb26470c5e363bcf14adebdf118310dcd65
ctliu <chiatungliu@gmail.com>
2018-04-30 18:18:10 +0800

    added checker to Add.js to check if same data has already exist before adding into DB

--- APIs/Add.js
+++ APIs/Add.js
@@ -20,6 +20,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 // adds an area to an owner
 add.post('/add/area', unlencodedParser, function(req, res) {
   var d = new Date();
+  var checker = false;
 
   var params = {
     TableName: "Areas",
@@ -32,19 +33,49 @@ add.post('/add/area', unlencodedParser, function(req, res) {
     }
   };
 
-  console.log("Adding a new item...");
-  docClient.put(params, function(err, data) {
+  var params_check = {
+    TableName: "Areas"
+  }
+
+  docClient.scan(params_check, onScan);
+
+  function onScan(err, data) {
     if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      console.log("Added item:", JSON.stringify(data, null, 2));
+      console.log("Scan succeeded.");
+      data.Items.forEach(function(items) {
+        if (items.name == req.body.name) {
+          checker = true;
+        }
+      });
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      }
     }
-  });
+
+    if (checker ==  false) {
+      docClient.put(params, function(err, data) {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+          res.send("Added " + req.body.name + " to owner");
+        }
+      });
+    }
+    else {
+      res.send("Area name already in use, please try another one");
+    }
+  }
 });
 
 // adds a sensorGroup to an area
 add.post('/add/sensorGroup', unlencodedParser, function(req, res) {
   var d = new Date();
+  var checker = false;
 
   var params = {
     TableName: "Sensor_Group",
@@ -57,19 +88,49 @@ add.post('/add/sensorGroup', unlencodedParser, function(req, res) {
     }
   };
 
-  console.log("Adding a new item...");
-  docClient.put(params, function(err, data) {
+  var params_check = {
+    TableName: "Sensor_Group"
+  }
+
+  docClient.scan(params_check, onScan);
+
+  function onScan(err, data) {
     if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      console.log("Added item:", JSON.stringify(data, null, 2));
+      console.log("Scan succeeded.");
+      data.Items.forEach(function(items) {
+        if (items.name == req.body.name) {
+          checker = true;
+        }
+      });
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      }
     }
-  });
+
+    if (checker ==  false) {
+      docClient.put(params, function(err, data) {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+          res.send("Added " + req.body.name + " to area");
+        }
+      });
+    }
+    else {
+      res.send("Sensor Group name already in use, please try another one");
+    }
+  }
 });
 
 // adds a sensor to a sensor group
 add.post('/add/sensor', unlencodedParser, function(req, res) {
   var d = new Date();
+  var checker = false;
 
   var params = {
     TableName: "Sensors",
@@ -83,14 +144,43 @@ add.post('/add/sensor', unlencodedParser, function(req, res) {
     }
   };
 
-  console.log("Adding a new item...");
-  docClient.put(params, function(err, data) {
+  var params_check = {
+    TableName: "Sensors"
+  }
+
+  docClient.scan(params_check, onScan);
+
+  function onScan(err, data) {
     if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      console.log("Added item:", JSON.stringify(data, null, 2));
+      console.log("Scan succeeded.");
+      data.Items.forEach(function(items) {
+        if (items.name == req.body.name) {
+          checker = true;
+        }
+      });
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      }
     }
-  });
+
+    if (checker ==  false) {
+      docClient.put(params, function(err, data) {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+          res.send("Added " + req.body.name + " to sensor group");
+        }
+      });
+    }
+    else {
+      res.send("Sensor name already in use, please try another one");
+    }
+  }
 });
 
 module.exports = add;
