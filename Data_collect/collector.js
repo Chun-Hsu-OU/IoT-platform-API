ebde9e93f4ce57c5f1878be8b07b314ace7e2086
ctliu <chiatungliu@gmail.com>
2018-05-31 02:10:13 +0800

    added lora to collector

--- Data_collect/collector.js
+++ Data_collect/collector.js
@@ -1,7 +1,9 @@
 var nthu_roof = require('./Areas/nthu_roof');
+var nthu_roof_lora = require('./Areas/nthu_roof_lora');
 var happyfarming = require('./Areas/happyfarming');
 var niu = require('./Areas/niu');
 
 nthu_roof.start();
+nthu_roof_lora.start();
 happyfarming.start();
 niu.start();
