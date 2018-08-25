79a3513eaa02170ce9d96aaab52c440fc7d35a15
Chun-Hsu-OU <a9638512@gmail.com>
2018-08-25 23:00:38 +0800

    7d2用到舊版的code，另外寫一個js給他

--- Data_collect/collector.js
+++ Data_collect/collector.js
@@ -3,9 +3,11 @@ var nthu_roof_lora = require('./Areas/nthu_roof_lora');
 var happyfarming = require('./Areas/happyfarming');
 var niu = require('./Areas/niu');
 var LoRa = require('./Areas/LoRa');
+var Hualien_7d2 = require('./Areas/Hualien_7d2');
 
 nthu_roof.start();
 nthu_roof_lora.start();
 happyfarming.start();
 niu.start();
 LoRa.start();
+Hualien_7d2.start();
