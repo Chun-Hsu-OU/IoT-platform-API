6107f2040e509209bc0775ed2f2c69e7a6115eba
Chun-Hsu-OU <a9638512@gmail.com>
2018-09-26 16:53:48 +0800

    修改雲林古坑收資料

--- Data_collect/Areas/Yunlin.js
+++ Data_collect/Areas/Yunlin.js
@@ -35,15 +35,15 @@ var start = function() {
       var sensor_data = JSON.parse(msg_temp);
 
       if (sensor_data['macAddr'] == 'hsnl1') {
-        if(sensor_data['sensortype'] == 'Air'){
+        if(sensor_data['Sensortype'] == 'Air'){
           methods.save_data('AIR_TEMPERATURE', sensor_data.Airtemp, '8985ce9f-6074-4d37-b37a-f1e458558852');
           methods.save_data('AIR_HUMIDITY', sensor_data.Airhum, 'd9ebfa78-13de-4522-a82d-3d6b04285d7b');
-        }else if(sensor_data['sensortype'] == 'Soil'){
+        }else if(sensor_data['Sensortype'] == 'Soil'){
           methods.save_data('SOIL_TEMPERATURE', sensor_data.Soiltemp, 'a175e55e-d2e0-493f-8fba-00591fc9dccc');
           methods.save_data('SOIL_HUMIDITY', sensor_data.Soilhum, '58c71479-a373-453d-833f-dc8f8e1a0ee7');
-        }else if(sensor_data['sensortype'] == 'Light'){
+        }else if(sensor_data['Sensortype'] == 'Light'){
           methods.save_data('LIGHT_INTENSITY', sensor_data.Lightlux, 'fa175b88-1b7d-46af-a2a7-ea3e9106d156');
-        }else if(sensor_data['sensortype'] == 'Battery'){
+        }else if(sensor_data['Sensortype'] == 'Battery'){
           methods.save_data('BATTERY_VOLTAGE', sensor_data.Batteryvoltage, '8a7357d6-ff05-486e-bdc9-426d5db9f1b4');
         } 
       }
