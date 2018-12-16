5128db7de81d6cd1fbe9ebdccfd5f4e5f4ec283f
Chun-Hsu-OU <a9638512@gmail.com>
2018-12-16 17:53:34 +0800

    增加pm2.5數據解析

---
+++ Data_collect/Areas/pm2_5.js
@@ -0,0 +1,70 @@
+const YAML = require('yamljs');
+const fs = require("fs");
+var path = require('path');
+var mqtt = require("mqtt");
+
+var methods = require('../methods');
+
+var record_time = 0;//紀錄上一筆時間，才能計算下一筆隔多久
+var time_period = 12*60*1000;//間隔多長再收下一筆，ex: 12分鐘
+
+// file为文件所在路径
+var doc = YAML.parse(fs.readFileSync(path.join(__dirname, '../../config') + '/secrets.yml').toString());
+var meter_addr = YAML.parse(fs.readFileSync(path.join(__dirname, '../../config') + '/pm2_5.yml').toString());
+
+var option = {
+    port: doc.MQTT_server1.port,
+    clientId: "pm2_5",
+    username: doc.MQTT_server1.HSNL_user,
+    password: doc.MQTT_server1.HSNL_password
+};
+
+var topic = "#";
+var client = mqtt.connect('mqtt://' + doc.MQTT_server1.IP, option);
+
+var start = function() {
+    //mqtt
+    client.on("connect", function() {
+        // console.log("Subscribing TOPIC: " + topic);
+        client.subscribe(topic);
+    });
+
+    client.on("message", function(topic, msg) {
+        var msg_temp = msg.toString();
+
+        if (msg_temp.startsWith("[{")) {
+            var sensor_data = JSON.parse(msg_temp)[0];
+            for(let i=0; i < meter_addr.macAddr.length; i++){
+                if(sensor_data.macAddr == meter_addr.macAddr[i]){
+                    console.log(sensor_data);
+                    handle(sensor_data.data, sensor_data.macAddr, sensor_data.time);
+                }
+            }
+        }
+    });
+}
+
+async function handle(data, macAddr, time){
+
+    var converted_time = new Date(time).getTime();
+    // console.log("現在時間： "+converted_time);
+    // console.log("紀錄時間： "+record_time);
+
+    if((converted_time - record_time) >= time_period){
+        //記錄時間
+        record_time = converted_time;
+
+        var raw_data = data.slice(12,16);
+        // console.log("raw: " + raw_data);
+        var pm2_5 = parseInt(raw_data, 16);
+        // console.log("pm2.5: " + pm2_5);
+        // console.log("------------------------------------");
+        
+        var id = await methods.searchId(macAddr, "PM2_5", "1");
+        methods.save_data("PM2_5", pm2_5, id);
+    }
+    
+}
+
+// start();
+module.exports.start = start;
--- Data_collect/collector.js
+++ Data_collect/collector.js
@@ -6,6 +6,7 @@ var LoRa = require('./Areas/LoRa');
 var Yunlin = require('./Areas/Yunlin');
 var water_meter = require('./Areas/water_meter');
 var electric_meter = require('./Areas/electric_meter');
+var pm2_5 = require('./Areas/pm2_5');
 
 nthu_roof.start();
 nthu_roof_lora.start();
@@ -15,3 +16,4 @@ LoRa.start();
 Yunlin.start();
 water_meter.start();
 electric_meter.start();
+pm2_5.start();
