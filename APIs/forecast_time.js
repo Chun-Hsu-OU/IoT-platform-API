b3543d1437405e9800f353c07cd556bf8a7257f6
Chun-Hsu-OU <a9638512@gmail.com>
2019-03-12 21:25:00 +0800

    取光照度時間調整

--- APIs/forecast_time.js
+++ APIs/forecast_time.js
@@ -40,49 +40,49 @@ forcast_time.get('/forecast_time/:uuid/:sensorId', function(req, res) {
     if(hour == 8){
       if(minute >= 30){
         //拿當天8:00~8:30
-        console.log(">= 8:30");
+        // console.log(">= 8:30");
         d.setMinutes(30);
-        console.log(d);
+        // console.log(d);
         to = d.getTime() - (8*60*60*1000);
 
         d.setMinutes(0);
-        console.log(d);
+        // console.log(d);
         from = d.getTime() - (8*60*60*1000);
       }else{
         //拿前一天8:00~8:30
-        console.log("< 8:30");
+        // console.log("< 8:30");
         var day = d.getDate();
         d.setDate(day-1);
         d.setHours(8, 30, 0);
-        console.log(d);
+        // console.log(d);
         to = d.getTime() - (8*60*60*1000);
 
         d.setMinutes(0);
-        console.log(d);
+        // console.log(d);
         from = d.getTime() - (8*60*60*1000);
       }
     }else{
       //拿當天8:00~8:30
-      console.log("> 8點");
+      // console.log("> 8點");
       d.setHours(8, 30, 0);
-      console.log(d.getTime());
+      // console.log(d.getTime());
       to = d.getTime() - (8*60*60*1000);
       
       d.setHours(8, 0, 0);
-      console.log(d.getTime());
+      // console.log(d.getTime());
       from = d.getTime() - (8*60*60*1000);
     }
   }else{
     //拿前一天8:00~8:30
-    console.log("< 8點");
+    // console.log("< 8點");
     var day = d.getDate();
     d.setDate(day-1);
     d.setHours(8, 30, 0);
-    console.log(d);
+    // console.log(d);
     to = d.getTime() - (8*60*60*1000);
 
     d.setMinutes(0);
-    console.log(d);
+    // console.log(d);
     from = d.getTime() - (8*60*60*1000);
   }
 
