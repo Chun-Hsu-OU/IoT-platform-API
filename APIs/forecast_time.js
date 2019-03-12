2e7e173c50c02ecbea3534ba0d8084c3182d81b5
Chun-Hsu-OU <a9638512@gmail.com>
2019-03-12 16:19:56 +0800

    測試預測澆水量

--- APIs/forecast_time.js
+++ APIs/forecast_time.js
@@ -31,7 +31,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 
 // 拿一個sensor最新的數據
 forcast_time.get('/forecast_time/:uuid/:sensorId', function(req, res) {
-  var d = new Date();
+  var d = new Date()+(8*60*60*1000);
   var hour = d.getHours();
   var minute = d.getMinutes();
   var from = 0;
