e00b2b3aa81dcf21163e1270aa5e8cfb31e4b480
Chun-Hsu-OU <a9638512@gmail.com>
2019-07-11 12:23:03 +0800

    將電池電壓和光照度偵測結合進去

--- Detect_abnormal/detect.js
+++ Detect_abnormal/detect.js
@@ -90,38 +90,6 @@ async function cycle_detect(){
     }else{
         console.log("大平窩 數據中斷 無法偵測!");
     }
-
-    //屏東江夏
-    //檢查所有sensorhub下的sensor都有資料再進行偵測
-    // var check_jiangxia = true;
-    // for(let group_num=0; group_num < sensorhub.jiangxia.length; group_num++){
-    //     var sensors_info = await methods.httpGet(api_url+"api/sensors_in_group/"+sensorhub.jiangxia[group_num], token);    
-    //     for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
-    //         var sensor = sensors_info.Items[sensor_num];
-    //         //檢查sensorhub裡要偵測的種類是否有資料
-    //         for(let type_num=0; type_num<sensortype.jiangxia.length; type_num++){
-    //             if(sensor.sensorType == sensortype.jiangxia[type_num]){
-    //                 var result = await methods.check_working(sensor.sensorType, sensor.sensorId, token);
-    //                 if(result == "No data"){
-    //                     check_jiangxia = false;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    // if(check_jiangxia){
-    //     console.log("江夏 數據穩定!");
-    //     //土壤溫度 A1
-    //     var period_jiangxia = await methods.cal_period("SOIL_TEMPERATURE", "4d011db6-72b9-43b7-a83e-3b656ca509a9", token);
-    //     console.log(period_jiangxia);
-    //     detect_jiangxia(period_jiangxia[1]);
-    //     setInterval(function(){
-    //         detect_jiangxia(period_jiangxia[1]);
-    //     }, period_jiangxia[1]*60*60*1000);
-    // }else{
-    //     console.log("江夏 數據中斷 無法偵測!");
-    // }
     
 }
 
@@ -132,8 +100,6 @@ async function detect_pingtung(period) {
     var d = new Date();
     var toEpoch = d.getTime();
     var fromEpoch = toEpoch - (period*60*60*1000);
-    // var toEpoch = 1547910000000;
-    // var fromEpoch = 1547740800000;
     
     //輪到判斷哪個type
     for(let type_num=0; type_num<sensortype.pingtung.length; type_num++){
@@ -142,33 +108,69 @@ async function detect_pingtung(period) {
         //儲存同一種類sensor的所有斜率資料
         var multi_line_slopes = [];
 
+        //BATTERY_VOLTAGE使用
+        voltages_of_each_batteries = [];
+        battery_hub_name = [];
+        //----------------------------
+
         //挑出sensorhub中一樣type的sensor，檢查是否異常
         for(let group_num=0; group_num < sensorhub.pingtung.length; group_num++){
             var hub_name = await methods.httpGet(api_url+"api/sensor_group/name/"+sensorhub.pingtung[group_num], token);
             console.log("hub_name: "+hub_name);
             var sensors_info = await methods.httpGet(api_url+"api/sensors_in_group/"+sensorhub.pingtung[group_num], token);
             
-            for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
-                var sensor = sensors_info.Items[sensor_num];
-                
-                //判斷是要偵測的type就開始算sensor在這個時間區段的斜率
-                if(sensor.sensorType == sensortype.pingtung[type_num]){
-                    var one_line_slopes = await methods.httpGet(api_url + 'api/linear/' + period +'/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch, token);
-                    //檢查sensor是否有數據
-                    if(one_line_slopes != "No data"){
-                        methods.add_name_and_type(one_line_slopes, hub_name+" "+sensor.num, sensor.sensorType);
-                        multi_line_slopes.push(one_line_slopes);
+            if(sensortype.pingtung[type_num] == "BATTERY_VOLTAGE"){
+
+                for(let sensor_num=0; sensor_num<sensors_info.Items.length; sensor_num++){
+                    var sensor = sensors_info.Items[sensor_num];
+                    if(sensor.sensorType == "BATTERY_VOLTAGE"){
+                        var voltages = await methods.httpGet(api_url + 'api/sensors_in_timeinterval/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch + '?token=' + token);
+                        if(voltages.Count > 0){
+                            var one_voltages_set = [];
+                            for(let k=0; k<voltages.Items.length; k++){
+                                one_voltages_set.push(voltages.Items[k]);
+                            }
+                            voltages_of_each_batteries.push(one_voltages_set);
+                            //當有電壓值時，另外紀錄sensor hub名稱
+                            battery_hub_name.push(hub_name);
+                        }
                     }
                 }
+
+            }else{ //土壤 ＆ 光照
+
+                for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
+                    var sensor = sensors_info.Items[sensor_num];
+                    
+                    //判斷是要偵測的type就開始算sensor在這個時間區段的斜率
+                    if(sensor.sensorType == sensortype.pingtung[type_num]){
+                        var one_line_slopes = await methods.httpGet(api_url + 'api/linear/' + period +'/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch, token);
+                        //檢查sensor是否有數據
+                        if(one_line_slopes != "No data"){
+                            methods.add_name_and_type(one_line_slopes, hub_name+" "+sensor.num, sensor.sensorType);
+                            multi_line_slopes.push(one_line_slopes);
+                        }
+                    }
+                }
+
             }
+
+            
         }
-        console.log(sensortype.pingtung[type_num]);
-        console.log();
-        console.log(multi_line_slopes);
-        console.log();
+        
 
         //開始偵測異常
-        methods.filterOutlier(multi_line_slopes, "火龍果", sensortype.pingtung[type_num], token);
+        if(sensortype.pingtung[type_num] == "BATTERY_VOLTAGE"){
+            methods.check_voltage(voltages_of_each_batteries, "火龍果", battery_hub_name, token);
+        }else if(sensortype.pingtung[type_num] == "LIGHT_INTENSITY"){
+            methods.check_light(multi_line_slopes, "火龍果", token);
+        }else{
+            console.log(sensortype.pingtung[type_num]);
+            console.log();
+            console.log(multi_line_slopes);
+            console.log();
+            methods.filterOutlier(multi_line_slopes, "火龍果", sensortype.pingtung[type_num], token);
+        }
     }
 }
 
@@ -187,79 +189,68 @@ async function detect_dapingwo(period) {
         //儲存同一種類sensor的所有斜率資料
         var multi_line_slopes = [];
 
+        //BATTERY_VOLTAGE使用
+        voltages_of_each_batteries = [];
+        battery_hub_name = [];
+        //----------------------------
+
         //挑出sensorhub中一樣type的sensor，檢查是否異常
         for(let group_num=0; group_num < sensorhub.dapingwo.length; group_num++){
             var hub_name = await methods.httpGet(api_url+"api/sensor_group/name/"+sensorhub.dapingwo[group_num], token);
             console.log("hub_name: "+hub_name);
             var sensors_info = await methods.httpGet(api_url+"api/sensors_in_group/"+sensorhub.dapingwo[group_num], token);
             
-            for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
-                var sensor = sensors_info.Items[sensor_num];
-                
-                //判斷是要偵測的type就開始算sensor在這個時間區段的斜率
-                if(sensor.sensorType == sensortype.dapingwo[type_num]){
-                    var one_line_slopes = await methods.httpGet(api_url + 'api/linear/' + period +'/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch, token);
-                    //檢查sensor是否有數據
-                    if(one_line_slopes != "No data"){
-                        methods.add_name_and_type(one_line_slopes, hub_name+" "+sensor.num, sensor.sensorType);
-                        multi_line_slopes.push(one_line_slopes);
+            if(sensortype.dapingwo[type_num] == "BATTERY_VOLTAGE"){
+
+                for(let sensor_num=0; sensor_num<sensors_info.Items.length; sensor_num++){
+                    var sensor = sensors_info.Items[sensor_num];
+                    if(sensor.sensorType == "BATTERY_VOLTAGE"){
+                        var voltages = await methods.httpGet(api_url + 'api/sensors_in_timeinterval/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch + '?token=' + token);
+                        if(voltages.Count > 0){
+                            var one_voltages_set = [];
+                            for(let k=0; k<voltages.Items.length; k++){
+                                one_voltages_set.push(voltages.Items[k]);
+                            }
+                            voltages_of_each_batteries.push(one_voltages_set);
+                            //當有電壓值時，另外紀錄sensor hub名稱
+                            battery_hub_name.push(hub_name);
+                        }
                     }
                 }
+
+            }else{ //土壤 ＆ 光照
+
+                for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
+                    var sensor = sensors_info.Items[sensor_num];
+                    
+                    //判斷是要偵測的type就開始算sensor在這個時間區段的斜率
+                    if(sensor.sensorType == sensortype.dapingwo[type_num]){
+                        var one_line_slopes = await methods.httpGet(api_url + 'api/linear/' + period +'/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch, token);
+                        //檢查sensor是否有數據
+                        if(one_line_slopes != "No data"){
+                            methods.add_name_and_type(one_line_slopes, hub_name+" "+sensor.num, sensor.sensorType);
+                            multi_line_slopes.push(one_line_slopes);
+                        }
+                    }
+                }
+
             }
+
         }
-        console.log(sensortype.dapingwo[type_num]);
-        console.log();
-        console.log(multi_line_slopes);
-        console.log();
 
         //開始偵測異常
-        methods.filterOutlier(multi_line_slopes, "大平窩", sensortype.dapingwo[type_num], token);
+        
+        //開始偵測異常
+        if(sensortype.dapingwo[type_num] == "BATTERY_VOLTAGE"){
+            methods.check_voltage(voltages_of_each_batteries, "大平窩", battery_hub_name, token);
+        }else if(sensortype.dapingwo[type_num] == "LIGHT_INTENSITY"){
+            methods.check_light(multi_line_slopes, "大平窩", token);
+        }else{
+            console.log(sensortype.dapingwo[type_num]);
+            console.log();
+            console.log(multi_line_slopes);
+            console.log();    
+            methods.filterOutlier(multi_line_slopes, "大平窩", sensortype.dapingwo[type_num], token);
+        }
     }
 }
-
-// async function detect_jiangxia(period) {
-//     //每次request都要帶token才能拿資料
-//     var token = await methods.getToken();
-
-//     var d = new Date();
-//     var toEpoch = d.getTime();
-//     // var toEpoch = 1552108800000;
-//     var fromEpoch = toEpoch - (period*60*60*1000);
-
-//     //輪到判斷哪個type
-//     for(let type_num=0; type_num<sensortype.jiangxia.length; type_num++){
-        
-//         console.log("----------------------------------");
-//         //儲存同一種類sensor的所有斜率資料
-//         var multi_line_slopes = [];
-
-//         //挑出sensorhub中一樣type的sensor，檢查是否異常
-//         for(let group_num=0; group_num < sensorhub.jiangxia.length; group_num++){
-//             var hub_name = await methods.httpGet(api_url+"api/sensor_group/name/"+sensorhub.jiangxia[group_num], token);
-//             console.log("hub_name: "+hub_name);
-//             var sensors_info = await methods.httpGet(api_url+"api/sensors_in_group/"+sensorhub.jiangxia[group_num], token);
-            
-//             for(let sensor_num=0; sensor_num < sensors_info.Items.length; sensor_num++){
-//                 var sensor = sensors_info.Items[sensor_num];
-                
-//                 //判斷是要偵測的type就開始算sensor在這個時間區段的斜率
-//                 if(sensor.sensorType == sensortype.jiangxia[type_num]){
-//                     var one_line_slopes = await methods.httpGet(api_url + 'api/linear/' + period +'/' + sensor.sensorType + '/' + sensor.sensorId + '/' + fromEpoch + '/' + toEpoch, token);
-//                     //檢查sensor是否有數據
-//                     if(one_line_slopes != "No data"){
-//                         methods.add_name_and_type(one_line_slopes, hub_name+" "+sensor.num, sensor.sensorType);
-//                         multi_line_slopes.push(one_line_slopes);
-//                     }
-//                 }
-//             }
-//         }
-//         console.log(sensortype.jiangxia[type_num]);
-//         console.log();
-//         console.log(multi_line_slopes);
-//         console.log();
-
-//         //開始偵測異常
-//         methods.filterOutlier(multi_line_slopes, "江夏", sensortype.jiangxia[type_num], token);
-//     }
-// }
-
--- Detect_abnormal/methods.js
+++ Detect_abnormal/methods.js
@@ -177,6 +177,133 @@ function area_to_uuid(area_name){
     return uuid;
 }
 
+//抓出掉4%電的電壓
+function check_voltage(voltages_of_each_batteries, area, hub_name, token){
+    // console.log(voltages_of_each_batteries);
+
+    var all_voltages = [];
+    for(let i=0; i<voltages_of_each_batteries.length; i++){
+        for(let j=0; j<voltages_of_each_batteries[i].length; j++){
+            if(voltages_of_each_batteries[i][j].value != null){
+                all_voltages.push(voltages_of_each_batteries[i][j].value);
+            }
+        }
+    }
+    // console.log(all_voltages);
+
+    var sort_voltages = Array_Sort_Numbers(all_voltages);
+    //挑出最大值(滿電電壓)
+    var full_voltage = sort_voltages[sort_voltages.length - 1];
+    // console.log("滿電: "+full_voltage);
+
+    var dangerous_voltage = full_voltage - full_voltage * 0.04;
+    // console.log("危險電壓: "+dangerous_voltage);
+
+    //挑出低於 "危險電壓" 的sensor
+    for(let i=0; i<voltages_of_each_batteries.length; i++){
+        for(let j=0; j<voltages_of_each_batteries[i].length; j++){
+            if(voltages_of_each_batteries[i][j].value != null){
+                if(voltages_of_each_batteries[i][j].value < dangerous_voltage){
+
+                    //要儲存的資料
+                    var save_info = {};
+                    save_info.area = area;
+                    save_info.sensor_type = "BATTERY_VOLTAGE";
+                    save_info.sensor_name = hub_name[i];
+                    save_info.sensorId = voltages_of_each_batteries[i][j].sensorId;
+                    save_info.from_time = voltages_of_each_batteries[i][j].timestamp - 30*60*1000;
+                    save_info.to_time = voltages_of_each_batteries[i][j].timestamp + 30*60*1000;          
+
+                    // state: 4 -> 電池電壓異常
+                    save_info.state = 4;
+                    save_abnormal_data(save_info, token);
+                    
+                    //傳送推播通知
+                    var messege = Convert_type_to_Ch("BATTERY_VOLTAGE") + "感測器 " + hub_name[i] + "\n在 " + 
+                                timeConverter(voltages_of_each_batteries[i][j].timestamp+8*60*60*1000 - 30*60*1000) + "~" + timeConverter(voltages_of_each_batteries[i][j].timestamp+8*60*60*1000 + 30*60*1000) +
+                                " 電池電壓異常\n可能會造成電力不足，之後有中斷數據的風險，需至現場檢查並維修";
+                    query_and_send_to_fcmTokens(area_to_uuid(area), messege);
+                }
+            }
+        }
+    }
+}
+
+//------------------------------------------------------
+
+//抓出平均正常值的5%以下的光照度
+function check_light(multi_line_slopes, area, token){
+    console.log(multi_line_slopes);
+
+    var col_length = multi_line_slopes[0].length;
+
+    //一次跑一組(同一行)相同時間sensors偵測異常趨勢
+    for(let col=0; col<col_length; col++){
+        var same_interval_data = [];
+
+        for(let row=0; row<multi_line_slopes.length; row++){
+            same_interval_data.push(multi_line_slopes[row][col]);
+        }
+        console.log(same_interval_data);
+
+        // 將光照度 >= 500分一群， < 500 分一群(可能異常)
+        var normal_values = [];
+        var maybe_abnormal_values = [];
+
+        for(let i=0; i<same_interval_data.length; i++){
+            if(same_interval_data[i].slope != "x"){
+                if(Math.abs(same_interval_data[i].slope) < 500){
+                    maybe_abnormal_values.push(same_interval_data[i]);
+                }else{
+                    normal_values.push(same_interval_data[i]);
+                }
+            }
+        }
+        
+        //算出平均正常值，挑出低於其5%以下的異常值，如果正常組沒有值(晚上)，就不用算
+        if(normal_values.length != 0){
+            var abs_slopes = convert_to_abs_slopes(normal_values);
+            var mean = Math.mean(abs_slopes);
+            var abnormal_check_point = mean * 0.05;
+
+            for(let i=0; i<maybe_abnormal_values.length; i++){
+                if(Math.abs(maybe_abnormal_values[i].slope) < abnormal_check_point){
+                    //要儲存的資料
+                    var save_info = {};
+                    save_info.area = area;
+                    save_info.sensor_type = "LIGHT_INTENSITY";
+                    save_info.sensor_name = maybe_abnormal_values[i].name;
+                    save_info.sensorId = maybe_abnormal_values[i].sensorId;
+                    save_info.from_time = maybe_abnormal_values[i].from;
+                    save_info.to_time = maybe_abnormal_values[i].to;         
+
+                    // state: 3 -> 光照度異常
+                    save_info.state = 3;
+                    save_abnormal_data(save_info, token);
+                    
+                    //傳送推播通知
+                    var messege = Convert_type_to_Ch("LIGHT_INTENSITY") + "感測器 " + maybe_abnormal_values[i].name + "\n在 " + 
+                                timeConverter(maybe_abnormal_values[i].from+8*60*60*1000) + "~" + timeConverter(maybe_abnormal_values[i].to+8*60*60*1000) +
+                                " 之間光照度異常\n可能是環境或感測器異常，需至現場檢查並維修";
+                    query_and_send_to_fcmTokens(area_to_uuid(area), messege);
+                }
+            }
+        }
+        
+    }
+}
+
+function convert_to_abs_slopes(normal_values){
+    var abs_slopes = [];
+    for(let i=0; i<normal_values.length; i++){
+        abs_slopes.push(Math.abs(normal_values[i].slope));
+    }
+
+    return abs_slopes;
+}
+
+//------------------------------------------------------
+
 /*-------------------------------------
 功用: 以四分位數從一堆斜率中挑出異常值
 Arg:
@@ -215,7 +342,7 @@ function filterOutlier(array, area, type, token){
                 save_info.from_time = all_info_slopes[i].from;
                 save_info.to_time = all_info_slopes[i].to;          
 
-                // state: 0 -> 過低，1 -> 過高 ，2 -> 資料不完整
+                // state: 2 -> 資料不完整
                 save_info.state = 2;
                 save_abnormal_data(save_info, token);
                 
@@ -224,12 +351,13 @@ function filterOutlier(array, area, type, token){
                             timeConverter(all_info_slopes[i].from+8*60*60*1000) + "~" + timeConverter(all_info_slopes[i].to+8*60*60*1000) +
                             " 之間數據量過少\n可能感測器狀況不穩，之後有中斷數據的風險，需至現場檢查並維修";
                 query_and_send_to_fcmTokens(area_to_uuid(area), messege);
-
-                //刪除元素
-                same_time_interval_slopes.splice(i, 1);
-                all_info_slopes.splice(i, 1);
             }
         }
+
+        //刪除(slope = x)，才不會影響到後面計算四分位數
+        same_time_interval_slopes = same_time_interval_slopes.filter(function(element, index, arr){
+            return element != "x";
+        });
 
         var q1 = Quartile_25(same_time_interval_slopes);
         var q3 = Quartile_75(same_time_interval_slopes);
@@ -250,9 +378,11 @@ function filterOutlier(array, area, type, token){
         var potential_anomaly = false;
         var threshold = 0;
         if(type == "SOIL_EC"){
-            threshold = 10;
+            threshold = 11;
+        }else if(type == "SOIL_HUMIDITY"){
+            threshold = 0.67;
         }else{
-            threshold = 1;
+            threshold = 0.6;
         }
 
         console.log("type: "+type);
@@ -279,7 +409,7 @@ function filterOutlier(array, area, type, token){
                     save_info.from_time = all_info_slopes[i].from;
                     save_info.to_time = all_info_slopes[i].to;
 
-                    // state: 0 -> 過低，1 -> 過高 ，2 -> 資料不完整
+                    
                     if(test_value < minValue){
                         console.log("異常值: "+test_value);
                         console.log("場域: "+area);
@@ -287,13 +417,14 @@ function filterOutlier(array, area, type, token){
                         console.log("從: "+timeConverter(all_info_slopes[i].from));
                         console.log("到: "+timeConverter(all_info_slopes[i].to));
                         console.log(all_info_slopes[i].sensorId+" 低於正常值");
+                        // state: 0 -> 過低
                         save_info.state = 0;
                         save_abnormal_data(save_info, token);
 
                         //傳送推播通知
                         var messege = Convert_type_to_Ch(all_info_slopes[i].type) + "感測器 " + all_info_slopes[i].name + "\n在 " + 
                                     timeConverter(all_info_slopes[i].from+8*60*60*1000) + "~" + timeConverter(all_info_slopes[i].to+8*60*60*1000) +
-                                    " 之間數據異常下降\n可能感測器有問題，需至現場檢查並維修";
+                                    " 之間數據異常下降\n可能是環境或感測器異常，需至現場檢查並維修";
                         query_and_send_to_fcmTokens(area_to_uuid(area), messege);
                     }
                     if(test_value > maxValue){
@@ -303,13 +434,14 @@ function filterOutlier(array, area, type, token){
                         console.log("從: "+timeConverter(all_info_slopes[i].from));
                         console.log("到: "+timeConverter(all_info_slopes[i].to));
                         console.log(all_info_slopes[i].sensorId+" 高於正常值");
+                        // state: 1 -> 過高
                         save_info.state = 1;
                         save_abnormal_data(save_info, token);
 
                         //傳送推播通知
                         var messege = Convert_type_to_Ch(all_info_slopes[i].type) + "感測器 " + all_info_slopes[i].name + "\n在 " + 
                                     timeConverter(all_info_slopes[i].from+8*60*60*1000) + "~" + timeConverter(all_info_slopes[i].to+8*60*60*1000) +
-                                    " 之間數據異常上升\n可能感測器有問題，需至現場檢查並維修";
+                                    " 之間數據異常上升\n可能是環境或感測器異常，需至現場檢查並維修";
                         query_and_send_to_fcmTokens(area_to_uuid(area), messege);
                     }
                 }
@@ -393,6 +525,10 @@ function Convert_type_to_Ch(type){
         ch = "土壤濕度";
     }else if(type == "SOIL_EC"){
         ch = "土壤電導度";
+    }else if(type == "BATTERY_VOLTAGE"){
+        ch = "電池電壓";
+    }else if(type == "LIGHT_INTENSITY"){
+        ch = "光照度";
     }
 
     return ch;
@@ -405,3 +541,5 @@ module.exports.cal_period = cal_period;
 module.exports.add_name_and_type = add_name_and_type;
 module.exports.filterOutlier = filterOutlier;
 module.exports.check_working = check_working;
+module.exports.check_voltage = check_voltage;
+module.exports.check_light = check_light;
